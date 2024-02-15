36d3607f7e32ddd0019ff1e02e9fd72b7688f826
faisalill <mfaisal.ilyas.d@gmail.com>
2024-02-15 23:06:27 +0530

    Update Repo

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,81 +1,91 @@
 <script>
-  import { T } from '@threlte/core'
-  import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
+  import { T, useThrelte, useTask } from '@threlte/core';
+  import { OrbitControls, useGltf, InstancedMesh, Instance } from '@threlte/extras';
+  import Satellite from '$lib/components/models/satellite.svelte';
+  import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
+  import { EquirectangularReflectionMapping, Vector3, DoubleSide } from 'three';
+  import { MeshSurfaceSampler } from 'three/addons/math/MeshSurfaceSampler.js';
+  import { onMount } from 'svelte';
+
+  const rgbeLoader = new RGBELoader();
+  const model = useGltf('/models/satellite.glb', { useDraco: true });
+  let sidePanel;
+  let isLoaded = false;
+  let sampler;
+  let sidePanelPositions = [];
+  let particlesCount = 10;
+  let shaderMaterialRef;
+  const { scene } = useThrelte();
+
+  onMount(async() => {
+    model.then((val) => {
+      sidePanel = val.nodes.side_panel;
+
+      sampler = new MeshSurfaceSampler(sidePanel).build();
+
+      for ( let i = 0; i < particlesCount; i++) {
+        const position = new Vector3();
+        sampler.sample(position);
+        sidePanelPositions.push(position);
+      }
+
+      isLoaded = true;
+    })
+    rgbeLoader.load('/envMap.hdr', (texture) => {
+      texture.mapping = EquirectangularReflectionMapping;
+      scene.environment = texture;
+    })
+
+  })
+
+  useTask((delta) =>{
+    if (isLoaded) {
+      shaderMaterialRef.uniforms.uTime.value += delta;
+    }
+  })
+
 </script>
 
 <T.PerspectiveCamera
   makeDefault
-  position={[-10, 10, 10]}
-  fov={15}
+  position={[0, 0, 380]}
 >
-  <OrbitControls
-    autoRotate
-    enableZoom={false}
-    enableDamping
-    autoRotateSpeed={0.5}
-    target.y={1.5}
-  />
+  <OrbitControls />
 </T.PerspectiveCamera>
 
-<T.DirectionalLight
-  intensity={0.8}
-  position.x={5}
-  position.y={10}
-/>
-<T.AmbientLight intensity={0.2} />
-
-<Grid
-  position.y={-0.001}
-  cellColor="#ffffff"
-  sectionColor="#ffffff"
-  sectionThickness={0}
-  fadeDistance={25}
-  cellSize={2}
-/>
-
-<ContactShadows
-  scale={10}
-  blur={2}
-  far={2.5}
-  opacity={0.5}
-/>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
+{#await model then value}
+<T.Points
+ geometry={value.nodes.side_panel.geometry}
 >
-  <T.Mesh
-    position.y={1.2}
-    position.z={-0.75}
-  >
-    <T.BoxGeometry />
-    <T.MeshStandardMaterial color="#0059BA" />
-  </T.Mesh>
-</Float>
+  <T.ShaderMaterial 
+    bind:ref={shaderMaterialRef}
+    uniforms={{
+      uTime: { value: 0.0 }
+    }}
+    vertexShader={`
+      uniform float uTime;
+      void main() {
+        gl_PointSize = 2.0;
+        vec3 pos = position;
+        float variation = sin(uTime * 8.0 + pos.y * 0.2) * 0.4;
+        pos.x += fract(variation * 2.0);
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+      }
+    `}
+    fragmentShader={`
+      varying vec3 vNormal;
+      uniform float uTime;
+      void main() {
+        vec3 col = vec3(1.0, 2.0, 3.0);
+        vec3 variation = 0.5 * col;
+        gl_FragColor = vec4(variation, 1.0);
+      }
+    `}
+    transparent
+    />
+</T.Points>
+{/await}
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[1.2, 1.5, 0.75]}
-    rotation.x={5}
-    rotation.y={71}
-  >
-    <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-    <T.MeshStandardMaterial color="#F85122" />
-  </T.Mesh>
-</Float>
+<!-- <Satellite position={[2,0,0]} /> -->
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[-1.4, 1.5, 0.75]}
-    rotation={[-5, 128, 10]}
-  >
-    <T.IcosahedronGeometry />
-    <T.MeshStandardMaterial color="#F8EBCE" />
-  </T.Mesh>
-</Float>
+
---
+++ src/lib/stores/geometry.js
@@ -0,0 +1,6 @@
+import { writable } from 'svelte/store';
+
+export const geometry = writable({
+  sidePanel: null
+});
+
